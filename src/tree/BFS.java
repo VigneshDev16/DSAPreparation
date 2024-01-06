be6002b4fb3644f6a4aa497400ecf9c9d6807a66
Vignesh <vigneshwaran.mutharasan@gmail.com>
2024-01-06 14:20:52 +0530

    BFS leetcode problems

---
+++ src/tree/BFS.java
@@ -0,0 +1,322 @@
+package tree;
+
+import java.util.*;
+
+public class BFS {
+
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(1);
+        node.right = new TreeNode(2);
+        BFS bfs = new BFS();
+        System.out.println(bfs.rightSideView(node));
+    }
+
+    //find level-order successor - google qn
+    public TreeNode findSuccessor(TreeNode root, int key){
+        if (root == null) {
+            return null;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            TreeNode currentNode = queue.poll();
+            if (currentNode.left != null) {
+                queue.offer(currentNode.left);
+            }
+            if (currentNode.right != null) {
+                queue.offer(currentNode.right);
+            }
+            if (currentNode.val == key) {
+                break;
+            }
+        }
+        return queue.peek();
+    }
+
+    //https://leetcode.com/problems/binary-tree-right-side-view/
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if(root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currNode = queue.poll();
+                if(i == levelSize-1){
+                    result.add(currNode.val);
+                }
+                if(currNode.left != null){
+                    queue.offer(currNode.left);
+                }
+                if(currNode.right != null){
+                    queue.offer(currNode.right);
+                }
+            }
+        }
+        return result;
+    }
+
+    //https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int levelSize = queue.size();
+            List<Integer> currLevelList = new ArrayList<>(levelSize);
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currNode = queue.poll();
+                currLevelList.add(currNode.val);
+                if(currNode.left != null){
+                    queue.offer(currNode.left);
+                }
+                if(currNode.right != null){
+                    queue.offer(currNode.right);
+                }
+            }
+
+            result.add(currLevelList);
+        }
+        int levels = result.size();
+        for (int i = 1; i <= levels; i++) {
+            if(i%2==0)
+            {
+                Collections.reverse(result.get(i-1));
+            }
+        }
+        return result;
+    }
+
+    //zigzagLevelOrder Alternate approach
+    public List<List<Integer>> zigzagLevelOrderAlt(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        boolean reverse = false;
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+            for (int i=0; i < levelSize; i++) {
+                if (!reverse) {
+                    TreeNode currentNode = queue.pollFirst();
+                    currentLevel.add(currentNode.val);
+                    if (currentNode.left != null) {
+                        queue.addLast(currentNode.left);
+                    }
+                    if (currentNode.right != null) {
+                        queue.addLast(currentNode.right);
+                    }
+                } else {
+                    TreeNode currentNode = queue.pollLast();
+                    currentLevel.add(currentNode.val);
+                    if (currentNode.right != null) {
+                        queue.addFirst(currentNode.right);
+                    }
+                    if (currentNode.left != null) {
+                        queue.addFirst(currentNode.left);
+                    }
+                }
+            }
+            reverse = !reverse;
+            result.add(currentLevel);
+        }
+        return result;
+    }
+
+    //https://leetcode.com/problems/average-of-levels-in-binary-tree
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        if(root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int levelSize = queue.size();
+            double averageLevel = 0;
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currNode = queue.poll();
+                averageLevel += currNode.val;
+                if(currNode.left != null){
+                    queue.offer(currNode.left);
+                }
+                if(currNode.right != null){
+                    queue.offer(currNode.right);
+                }
+            }
+            averageLevel = averageLevel / levelSize;
+            result.add(averageLevel);
+        }
+        return result;
+    }
+
+    //https://leetcode.com/problems/binary-tree-level-order-traversal/
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int levelSize = queue.size();
+            List<Integer> currLevelList = new ArrayList<>(levelSize);
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currNode = queue.poll();
+                currLevelList.add(currNode.val);
+                if(currNode.left != null){
+                    queue.offer(currNode.left);
+                }
+                if(currNode.right != null){
+                    queue.offer(currNode.right);
+                }
+            }
+
+            result.add(currLevelList); //result.add(0,currLevelList); -> https://leetcode.com/problems/binary-tree-level-order-traversal-ii
+        }
+        //Collections.reverse(result); -> https://leetcode.com/problems/binary-tree-level-order-traversal-ii
+        return result;
+    }
+
+    //https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+    public Node connect(Node root) {
+        if(root == null){
+            return root;
+        }
+        Node leftMost = root;
+        while(leftMost.left != null){
+            Node curr = leftMost;
+            while(curr != null){
+                curr.left.next = curr.right;
+                if(curr.next != null)
+                {
+                    curr.right.next = curr.next.left;
+                }
+                curr = curr.next;
+            }
+            leftMost = leftMost.left;
+        }
+        return root;
+    }
+
+    //https://leetcode.com/problems/cousins-in-binary-tree/
+    public boolean isCousins(TreeNode root, int x, int y) {
+        if(root == null){
+            return false;
+        }
+
+        TreeNode xx = findNode(root,x);
+        TreeNode yy = findNode(root,y);
+
+        return (level(root,xx,0) == level(root,yy,0) && (!isSibling(root,xx,yy)));
+    }
+
+    private boolean isSibling(TreeNode node, TreeNode x, TreeNode y) {
+        if (node==null)
+            return false;
+        boolean result = (node.left == x && node.right == y) || (node.left == y && node.right == x) || isSibling(node.left, x, y) || isSibling(node.right, x, y);
+        return result;
+    }
+
+    private int level(TreeNode node, TreeNode x,int lev) {
+        if(node == null)
+            return 0;
+        if (node == x)
+            return lev;
+        int l = level(node.left,x,lev+1);
+        if(l!=0)
+            return l;
+        return level(node.right,x,lev+1);
+    }
+
+    private TreeNode findNode(TreeNode node,int x) {
+        if(node == null)
+            return null;
+
+        if(node.val == x)
+            return node;
+
+        TreeNode left = findNode(node.left,x);
+        if(left!=null)
+            return left;
+        return findNode(node.right,x);
+    }
+
+    //https://leetcode.com/problems/symmetric-tree/
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+
+        while (!queue.isEmpty()){
+            TreeNode l = queue.poll();
+            TreeNode r = queue.poll();
+
+            if(l ==null && r==null){
+                continue;
+            }
+            if(l==null || r==null){
+                return false;
+            }
+            if(l.val != r.val){
+                return false;
+            }
+            queue.offer(l.left);
+            queue.offer(r.right);
+            queue.offer(l.right);
+            queue.offer(r.left);
+        }
+
+        return true;
+    }
+}
+
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
